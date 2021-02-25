681ce98d61d9b52909bce71e6ee60549d9d42d27
mhrehman <habibcomsats@gmail.com>
2021-02-25 20:39:19 +0000

    initial structure

---
+++ communication/client.py
@@ -0,0 +1,38 @@
+import socket
+
+HEADER  = 64
+
+PORT = 8000
+
+FORMAT = 'utf-8'
+
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+SERVER = "127.0.0.1"  
+
+ADDR = (SERVER,PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print(client)
+
+client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' '*(HEADER-len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
+
+send("Hello World")
+input()
+
+send("Hello Everyone")
+input()
+
+send("Hello Habib")
+
+send(DISCONNECT_MESSAGE)
